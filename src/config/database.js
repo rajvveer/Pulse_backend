8dba653c960e01834592bc1e5fed99a92e716963
Rajveer(Chiranjeev)Shekhawat <121601167+rajvveer@users.noreply.github.com>
2026-01-21 09:25:07 +0530

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -15,7 +15,6 @@ class DatabaseConfig {
 
       const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/pulse';
       
-      // UPDATED: Removed deprecated options
       const options = {
         maxPoolSize: parseInt(process.env.MONGO_OPTIONS_MAX_POOL_SIZE) || 10,
         serverSelectionTimeoutMS: parseInt(process.env.MONGO_OPTIONS_SERVER_SELECTION_TIMEOUT_MS) || 5000,
